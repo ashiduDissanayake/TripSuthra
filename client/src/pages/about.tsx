941d0123bcfc77b9f7f0d99dacdf37179dc3e3db
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-02 19:43:02 +0530

    chore: change tsx content

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -1,14 +1,7 @@
-import { title } from "@/components/primitives";
-import DefaultLayout from "@/layouts/default";
+import News from "./news";
 
 export default function DocsPage() {
   return (
-    <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>About</h1>
-        </div>
-      </section>
-    </DefaultLayout>
+    <News />
   );
 }
