5f140a5d6ff0089150114ece11aef945ca67bdfa
kin-lgtm <143951269+kin-lgtm@users.noreply.github.com>
2024-09-02 18:07:18 +0530

    FirstStep

---
+++ client/src/pages/destination.tsx
@@ -0,0 +1,13 @@
+import "./style.css";
+// Adjust the path to where your Image component is located
+
+const Destination = () => {
+  return (
+    <div>
+      <Image />
+      destinatioD
+    </div>
+  );
+};
+
+export default Destination;
