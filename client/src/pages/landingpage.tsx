9c602d2c28d9be8b1c1fbf960c6fdbe22fc2d08b
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-04 00:43:48 +0530

    fix: change code

--- client/src/pages/landingpage.tsx
+++ client/src/pages/landingpage.tsx
@@ -1,7 +1,4 @@
 import HeroSection from '@/components/landingpage/herosection'
-
-import React from 'react'
-
 import TrendingEvents from '@/components/landingpage/TrendingEvents';
 import TrendingDestinations from '@/components/landingpage/TrendingDestinations';
 
