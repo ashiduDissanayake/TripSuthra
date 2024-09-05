ef277f705bf1958542fff6c121f7eff063fd5828
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-05 22:10:21 +0530

    feat: add navbar and footer

--- client/src/pages/destination.tsx
+++ client/src/pages/destination.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import Hero from '@/components/destination/hero';
-import Trendingdestination from '@/components/destination/trendingdestinations';
-import DestinationCategory from '@/components/destination/destinationcategory';
-import Explore from '@/components/destination/explore';
-import App from '@/components/destination/appdownload';
+import React from "react";
+import Hero from "@/components/destination/hero";
+import Trendingdestination from "@/components/destination/trendingdestinations";
+import DestinationCategory from "@/components/destination/destinationcategory";
+import Explore from "@/components/destination/explore";
+import App from "@/components/destination/appdownload";
+import { Navbar } from "@/components/navbar";
+import Footer from "@/components/footer";
 
+Footer;
+Navbar;
 const Destination = (): JSX.Element => {
   return (
     <div>
+      <div className="pb-1">
+        <Navbar />
+      </div>
       <Hero />
       <Trendingdestination />
       <DestinationCategory />
       <Explore />
       <App />
+      <div className="pt-1">
+        <Footer />
+      </div>
     </div>
   );
-}
+};
 
 export default Destination;
